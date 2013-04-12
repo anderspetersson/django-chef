ad6d0389165995c601e0c62da8b25d02ee167fcb
Anders Petersson <me@anderspetersson.se>
2013-04-12 20:00:45 +0200

    Fixed typos in site recipes.

--- cookbooks/hosting/recipes/site.rb
+++ cookbooks/hosting/recipes/site.rb
@@ -24,6 +24,6 @@ script "Install Requirements for #{node[:site][:name]}" do
   user "#{node[:site][:user]}"
   group "#{node[:site][:group]}"
   code <<-EOH
-  /home/{node[:site][:user]}/sites/#{node[:site][:name]}/bin/pip install -r /home/{node[:site][:user]}/sites/#{node[:site][:name]}/src/#{node[:site][:name]}/#{node[:site][:requirements_file]}
+  /home/#{node[:site][:user]}/sites/#{node[:site][:name]}/bin/pip install -r /home/#{node[:site][:user]}/sites/#{node[:site][:name]}/src/#{node[:site][:name]}/#{node[:site][:requirements_file]}
   EOH
 end
